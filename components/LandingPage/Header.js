9472eb86ead36b9e4d2061c4b9f50ba75eda7873
Abhishek Jaiswal <iam001abhi@gmail.com>
2024-01-24 23:42:48 +0530

    did redux config and added the store

--- components/LandingPage/Header.js
+++ components/LandingPage/Header.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import CartIcon from '../Cart/CartIcon';
+import { UseSelector, useSelector } from 'react-redux';
+
 function Header() {
+    const cartItems=useSelector((store)=>store.cart.items);
     return (
         <div className="headerContainer  bg-gray-800 h-280 w-full mb-4">
             <div className="Header flex justify-between ">
@@ -16,8 +20,8 @@ function Header() {
                 </ul>
 
                 <ul className="flex  py-3">
-                    <li className="px-4  py-2 text-white font-mono text-lg font-bold hover:bg-gray-900 hover:rounded-md antialiased" >Login</li>
-                    <li className="px-4  py-2 text-white font-mono text-lg font-bold hover:bg-gray-900 hover:rounded-md antialiased">SignUp</li>
+                    <li className="px-4  py-2 text-white text-2xl  " ><i class="fa-solid fa-cart-shopping"></i> Cart( {cartItems.length} )</li>
+                    <li className="px-4  py-2 text-white font-mono text-lg font-bold hover:bg-gray-900 hover:rounded-md antialiased">SignIn</li>
                 </ul>
             </div>
 
